7cedfd7fb151db3b6280198c1c1a1950cf45c16a
jbukhari <joe.bukhari@gmail.com>
2026-04-13 23:04:47 -0400

    Vue (#3)
    * add vue source
    
    * add base vue app
    
    * add vue app file
    
    * Rename project to 'flaskvue' and update description
    
    * add pinia

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, send_file, render_template
+from flask_login import current_user, login_required
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
+Flask.jinja_options.update({'variable_start_string': '[[', 'variable_end_string': ']]'})
 
 @app.route("/")
-def index():
-    return render_template('index.html', user='me')
+def home():
+    return render_template('app.html', user='me')
