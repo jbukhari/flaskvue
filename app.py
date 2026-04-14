f64e2e117b49b755db6ed6feb4f47e6a2827a9da
jbukhari <jbukhari@gmail.com>
2026-04-14 14:35:58 -0400

    cleanup

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+"""Main router"""
+
 from flask import Flask, send_file, render_template
 from flask_login import current_user, login_required
 from flask_cors import CORS
@@ -6,6 +8,14 @@ app = Flask(__name__)
 CORS(app)
 Flask.jinja_options.update({'variable_start_string': '[[', 'variable_end_string': ']]'})
 
-@app.route("/")
-def home():
+@app.route('/')
+def index():
+    return render_template('index.html', user='me')
+
+@app.route('/app')
+def app():
     return render_template('app.html', user='me')
+
+@app.route('/api')
+def api():
+    pass
