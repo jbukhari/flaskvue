315d39c81fdd32b7c72f3f713daaa5c4141f7d75
jbukhari <jbukhari@gmail.com>
2026-07-04 09:49:23 -0400

    enable conditional JS source

--- app/app.py
+++ app/app.py
@@ -1,7 +1,8 @@
 # Routes are declared using both conventional Flask declaration and Flask-RESTX
 # declaration
 
-import json
+import os, json
+from pathlib import Path
 from flask import Flask, send_file, render_template, jsonify, request, Response, abort
 from flask_login import current_user, login_required
 from flask_cors import CORS
@@ -13,6 +14,16 @@ from app import models
 ### Conventional routes
 APP = Flask(__name__)
 CORS(APP)
+EXTERNAL_JS = not os.environ.get('EXTERNAL_JS') or False # default to loading JS from external sources
+INTERNAL_JS_PATH = '../static/js' 
+
+@APP.context_processor
+def supply_js_sources():
+    return {
+        'vue_src': 'https://unpkg.com/vue@3.5.29/dist/vue.esm-browser.js' if EXTERNAL_JS else f'{INTERNAL_JS_PATH}/vue_3.5.29_esm-browser.js',
+        'vue_devtools_src': 'https://unpkg.com/@vue/devtools-api@8.1.1/dist/vue-devtools-api.esm-browser.js' if EXTERNAL_JS else f'{INTERNAL_JS_PATH}/vue-devtools-api_8.1.1_esm-browser.js',
+        'pinia_src': 'https://unpkg.com/pinia@3.0.4/dist/pinia.esm-browser.js' if EXTERNAL_JS else f'{INTERNAL_JS_PATH}/pinia_3.0.4_esm-browser.js'
+    }
 
 @APP.route('/')
 def index():
