c09a463a39f6a2726e502a0777ba4257ddab77b7
jbukhari <joe.bukhari@gmail.com>
2026-07-13 12:18:33 -0400

    Download js module source command (#19)
    * download js module source command
    
    * move commands to own file

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 # Routes are declared using both conventional Flask declaration and Flask-RESTX
 # declaration
 
-import os, json
+import os, json, requests
 from pathlib import Path
 from flask import Flask, send_file, render_template, jsonify, request, Response, abort
 from flask_login import current_user, login_required
@@ -10,22 +10,31 @@ from flask_restx import Api, Resource, fields
 from flask_pydantic import validate
 from app.config import Config
 from app import models
+from app.commands import commands
 
-### Conventional routes
 APP = Flask(__name__)
+APP.register_blueprint(commands)
+Config.app = APP
 CORS(APP)
 INTERNAL_JS = getattr(Config, 'INTERNAL_JS', False)
 INTERNAL_JS_DIR = getattr(Config, 'INTERNAL_JS_PATH', None) if INTERNAL_JS else None
 
+### Context processor
 @APP.context_processor
 def supply_js_sources():
     # Where the JS libraries will be loaded from
-    return {
-        'vue_src': f'{INTERNAL_JS_DIR}/vue_3.5.29_esm-browser.js' if INTERNAL_JS else Config.VUE_SRC,
-        'vue_devtools_src': f'{INTERNAL_JS_DIR}/vue-devtools-api_8.1.1_esm-browser.js' if INTERNAL_JS else Config.VUE_DEVTOOLS_SRC,
-        'pinia_src': f'{INTERNAL_JS_DIR}/pinia_3.0.4_esm-browser.js' if INTERNAL_JS else Config.PINIA_SRC
+    
+    def relative_path(path):
+        # Return path relative to the file with the importmap
+        return str(Path(path).relative_to(Path('../templates/base.html').resolve(), walk_up=True)).replace('../app', '') # relative_to requires walkup but js import doesn't support walkup
+
+    return { 
+        'vue_src': relative_path(f'{INTERNAL_JS_DIR}/{Config.VUE_FN}') if INTERNAL_JS else Config.VUE_SRC,
+        'vue_devtools_src': relative_path(f'{INTERNAL_JS_DIR}/{Config.VUE_DEVTOOLS_FN}') if INTERNAL_JS else Config.VUE_DEVTOOLS_SRC,
+        'pinia_src': relative_path(f'{INTERNAL_JS_DIR}/{Config.PINIA_FN}') if INTERNAL_JS else Config.PINIA_SRC
     }
 
+### Conventional routes
 @APP.route('/')
 def index():
     return render_template('index.html', user='user')
@@ -34,7 +43,7 @@ def index():
 def main_app():
     return render_template('app.html', user='user')
 
-### API
+### API routes
 # Flask-RESTX
 API = Api(APP, doc='/api')
 NS = API.namespace('api', description='App API')
