9d221b1e647a692ec7e29b21482116c7ebffa80f
jbukhari <joe.bukhari@gmail.com>
2026-07-16 10:24:59 -0400

    move api routes to blueprint (#21)

--- app/app.py
+++ app/app.py
@@ -1,20 +1,20 @@
-# Routes are declared using both conventional Flask declaration and Flask-RESTX
-# declaration
+# Main Flask app. Routes that render templates or static pages are defined
+# here. Routes that return data are defined in api.py and imported via
+# Blueprint.
 
-import os, json, requests
+import json
 from pathlib import Path
-from flask import Flask, send_file, render_template, jsonify, request, Response, abort
+from flask import Flask, send_file, render_template, abort
 from flask_login import current_user, login_required
 from flask_cors import CORS
-from flask_restx import Api, Resource, fields
 from flask_pydantic import validate
 from app.config import Config
-from app import models
 from app.commands import commands
+from app.api import API
 
-APP = Flask(__name__)
+Config.app = APP = Flask(__name__)
 APP.register_blueprint(commands)
-Config.app = APP
+APP.register_blueprint(API.blueprint)
 CORS(APP)
 INTERNAL_JS = getattr(Config, 'INTERNAL_JS', False)
 INTERNAL_JS_DIR = getattr(Config, 'INTERNAL_JS_PATH', None) if INTERNAL_JS else None
@@ -34,7 +34,7 @@ def supply_js_sources():
         'pinia_src': relative_path(f'{INTERNAL_JS_DIR}/{Config.PINIA_FN}') if INTERNAL_JS else Config.PINIA_SRC
     }
 
-### Conventional routes
+### Routes
 @APP.route('/')
 def index():
     return render_template('index.html', user='user')
@@ -42,41 +42,3 @@ def index():
 @APP.route('/app')
 def main_app():
     return render_template('app.html', user='user')
-
-### API routes
-# Flask-RESTX
-API = Api(APP, doc='/api')
-NS = API.namespace('api', description='App API')
-
-# All routes defined below come after "api/", set by the namespace object
-@NS.route('')
-class Doc(Resource):
-    @NS.doc(description='API Documentation')
-    def get(self):
-        # Placeholder for the Swagger doc provided by Flask-RESTX, set using
-        # the "doc" argument to the Api object.
-        return {'message': 'The documentation is supposed to be supplied here by Flask-RESTX.'}
-
-@NS.route('/records')
-class Records(Resource):
-    @NS.doc(description='List records')
-    def get(self):
-        data = [{'id': 1}]
-        return (jsonify([]))
-    
-    @NS.doc(description='Create record')
-    @NS.expect(models.Record.model())
-    def post(self):
-        data = json.loads(request.data)
-        
-        if models.Record.model_validate(data):
-            return Response(status=201)
-        
-        abort(400)
-    
-@NS.route('/record')
-class Record(Resource):
-    @NS.doc(description='Get record')
-    def get(self):
-        data = {'id': 1}
-        return (data)
